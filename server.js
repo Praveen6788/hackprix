744f00f02962803db6cc16cd00db85c0229a404b
Praveen6788 <22951a6786@iare.ac.in>
2024-06-20 13:35:01 +0530

    minor change

--- server.js
+++ server.js
@@ -1,54 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const path = require('path');
-// const port = 3019;
-
-// const app = express();
-// app.use(express.static(__dirname));
-// app.use(express.urlencoded({ extended: true }));
-
-// mongoose.connect("mongodb://127.0.0.1:27017/LOGIN_INFO")
-// const db = mongoose.connection
-// db.once("open",()=>{
-//     console.log("Mongodb Connected Successfull")
-// })
-
-// const userschema = new mongoose.Schema({
-//     username: String,
-//     password: String,
-//     person: String
-// });
-
-// const Users = mongoose.model("data", userschema);
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, "login.html"));
-// });
-
-// app.post("/login", async (req, res) => {
-//     try {
-//         const { username, password, person } = req.body;
-//         const user = new Users({
-//             username,
-//             password,
-//             person
-//         });
-//         await user.save();
-//         console.log(user);
-//         res.redirect("home.html");
-//     } catch (err) {
-//         console.error("Error Saving User: ", err);
-//         res.status(500).send("Error Saving User");
-//     }
-// });
-
-// app.listen(port, () => {
-//     console.log("Server started on localhost for landing page login signup:", port);
-// });
-
-
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -75,7 +24,7 @@ const userSchema = new mongoose.Schema({
 const Users = mongoose.model("data", userSchema);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "login.html"));
+    res.sendFile(path.join(__dirname, "landingpage.html"));
 });
 
 app.get('/signup', (req, res) => {
